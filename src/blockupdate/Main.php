a9be7efe42dc4a2d82d03f25c23924285b142386
MyNET <megaco789@gmail.com>
2018-01-17 16:46:15 +0800

    Update Main.php

--- src/blockupdate/Main.php
+++ src/blockupdate/Main.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace blockupdate;
 
 use pocketmine\block\Block;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase{
 
@@ -10,22 +12,22 @@ class Main extends PluginBase{
     private $bypassBlocks = [];
     private $blockLeaveDecay = true;
 
-	  public function onEnable(){
-			  $this->saveResource('config.yml');
-			  self::$instance = $this;
-				$this->bypassBlocks = $this->getConfig()->get('Allow-Update');
+    public function onEnable(){
+	$this->saveResource('config.yml');
+	self::$instance = $this;
+	$this->bypassBlocks = $this->getConfig()->get('Allow-Update');
         $this->blockLeaveDecay = $this->getConfig()->get('Block-Leave-Decay');
-		    $this->getServer()->getPluginManager()->registerEvents(new BlockUpdateListener(), $this);
-				$this->getServer()->getLogger()->info('Enabled!');
-	  }
+        $this->getServer()->getPluginManager()->registerEvents(new BlockUpdateListener(), $this);
+	$this->getServer()->getLogger()->info("[BU] Enable!");
+    }
 
-		public static function getInstance(){
-		    return self::$instance;
-		}
+    public static function getInstance(){
+        return self::$instance;
+    }
 
-		public function canBypass(Block $block){
+    public function canBypass(Block $block){
         return in_array($block->getId(), $this->bypassBlocks);
-		}
+    }
 
     public function blockLeaveDecay(){
         return $this->blockLeaveDecay;
