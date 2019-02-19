8de4089723f68fb0feaed06bc5e0367996bc8d5f
MyNET <megaco789@gmail.com>
2019-02-20 01:59:28 +0800

    Update Main.php

--- src/blockupdate/Main.php
+++ src/blockupdate/Main.php
@@ -1,34 +1,43 @@
 <?php
 
+declare(strict_types=1);
+
 namespace blockupdate;
 
 use pocketmine\block\Block;
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\Server;
+use pocketmine\utils\Config;
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener{
 
     private static $instance = null;
     private $bypassBlocks = []; //config array
     private $blockLeaveDecay = true; 
+    
+    private const PREFIX = TextFormat::GREEN . "BlockUpdate" . TextFormat::GOLD . " > ";
 
-	  public function onEnable(){
-			  $this->saveResource('config.yml');
-			  self::$instance = $this;
-				$this->bypassBlocks = $this->getConfig()->get('Allow-Update'); //Config array
-        $this->blockLeaveDecay = $this->getConfig()->get('Block-Leave-Decay');
-		    $this->getServer()->getPluginManager()->registerEvents(new BlockUpdateListener(), $this);
-				$this->getServer()->getLogger()->info('Enabled!');
-	  }
-
-		public static function getInstance(){
-		    return self::$instance;
-		}
-
-		public function canBypass(Block $block){
+    public function onEnable(){
+        @mkdir($this->getDataFolder());
+        $this->saveDefaultConfig();
+	self::$instance = $this;
+        $this->bypassBlocks = $this->getConfig()->get("Allow-Update"); //Config array
+        $this->blockLeaveDecay = $this->getConfig()->get("Block-Leave-Decay");
+	$this->getServer()->getPluginManager()->registerEvents(new BlockUpdateListener(), $this);
+        $this->getLogger()->info(self:PREFIX . "Enabled!");
+   }
+	
+   public static function getInstance(){
+	return self::$instance;
+   }
+	
+   public function canBypass(Block $block){
         return in_array($block->getId(), $this->bypassBlocks);
-    }
-
-    public function blockLeaveDecay(){
+   }
+	
+   public function blockLeaveDecay(){
         return $this->blockLeaveDecay;
-    }
+   }
 }
