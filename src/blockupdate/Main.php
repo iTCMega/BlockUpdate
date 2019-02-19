621582cf457562d178e9fc69dd1d50f766d8e936
Maarten <Landy005@users.noreply.github.com>
2019-02-19 16:26:11 +0100

    Update Main.php

--- src/blockupdate/Main.php
+++ src/blockupdate/Main.php
@@ -11,20 +11,20 @@ class Main extends PluginBase{
     private $bypassBlocks = []; //config array
     private $blockLeaveDecay = true; 
 
-    public function onEnable(){
-	$this->saveResource('config.yml');
-	self::$instance = $this;
-	$this->bypassBlocks = $this->getConfig()->get('Allow-Update'); //Config array
+	  public function onEnable(){
+			  $this->saveResource('config.yml');
+			  self::$instance = $this;
+				$this->bypassBlocks = $this->getConfig()->get('Allow-Update'); //Config array
         $this->blockLeaveDecay = $this->getConfig()->get('Block-Leave-Decay');
-        $this->getServer()->getPluginManager()->registerEvents(new BlockUpdateListener(), $this);
-	$this->getServer()->getLogger()->info("[BU] Enable!");
-    }
+		    $this->getServer()->getPluginManager()->registerEvents(new BlockUpdateListener(), $this);
+				$this->getServer()->getLogger()->info('Enabled!');
+	  }
 
-    public static function getInstance(){
-        return self::$instance;
-    }
+		public static function getInstance(){
+		    return self::$instance;
+		}
 
-    public function canBypass(Block $block){
+		public function canBypass(Block $block){
         return in_array($block->getId(), $this->bypassBlocks);
     }
 
